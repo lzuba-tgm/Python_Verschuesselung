e42a9428a57e03ab405f8c04c59c9aff9d8ec757
Lukas Zuba <lzuba@student.tgm.ac.at>
2016-10-14 00:06:45 +0200

    Added main loop

--- threadverschluesselung/main.py
+++ threadverschluesselung/main.py
@@ -1,9 +1,25 @@
 from threading import *
+import sys
+
+class VersThr (Thread):
+    unver = ""
+    ver = ""
+    def __init__(self,unver):
+        Thread.__init__(self)
+        self.unver = unver
+
+    def run(self):
+        print "'" + str(self.unver) + "'\n"
+        self.ver = self.ver + self.unver
+
+
+#
 
 class startup(object):
     eingabe = ""
+    ausgabe = ""
     anzahl = 0
-    threads = []
+    mythreads = []
 
     def __init__(self):
         self.eingabe = raw_input("Geben Sie das zu verschluesselnde Wort ein \n")
@@ -12,29 +28,47 @@ class startup(object):
         #print self.anzahl
 
     def startver(self):
-        teil = []
-        for i in range(0,int(self.anzahl)):
-            #part = eingabe[]
-        qoute = len(self.eingabe) / int(self.anzahl)
-        print "Qoute: "+ str(qoute)
+        position = 0
+        qoute = float(len(self.eingabe)) / float(self.anzahl)
         for x in range(0,int(self.anzahl)):
-            thread = VerschluesselungThr(self.eingabe).start()
-            self.threads += [thread]
+            part = self.eingabe[int(position):int(position+qoute)]
+            position += float(qoute)
+            if len(part) != 0:
+                thread = ""
+                thread = VersThr(part)
+                thread.start()
+                self.mythreads.append(thread)
+        #print self.mythreads
+        for thread in self.mythreads:
+            print thread.unver
+            thread.join()
 
 
+"""
 
-class VerschluesselungThr (Thread):
-    unver = ""
-    ver = ""
-    def __init__(self,unver):
-        Thread.__init__(self)
-        self.unver = unver
-        print "bla: " + str(self.unver)
+thread = VersThr("ghg")
+thread.start()
+print "Bla: " + str(thread)
 
+"""
 
-    def run(self):
-        pass
+#Main Methode Haupt-Loop
 
+while True :
+    print "Geben Sie ein was Sie machen wollen"
+    ein = raw_input("1 => Verschluesseln  2 => Entschluesseln  3 => Exit \n")
 
-starting = startup()
-starting.startver()
+    try:
+        ein = int(ein)
+    except ValueError:
+        ein = 0
+
+    if ein == 1:
+        starting = startup()
+        starting.startver()
+    elif ein == 2:
+        print "Placeholder"
+    elif ein == 3:
+        sys.exit(0)
+    else:
+        print "Unbekannter Befehl"
